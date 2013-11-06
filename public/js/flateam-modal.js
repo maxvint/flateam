5f510fee31d805263789b1e0ebdcdacf6d95dfa6
yuwenhui <yuwenhui1986@163.com>
2013-11-06 18:02:48 +0800

    modify css and modal

--- public/js/flateam-modal.js
+++ public/js/flateam-modal.js
@@ -18,236 +18,6 @@
  * ========================================================= */
 
 
-!function ($) {
-
-	"use strict"; // jshint ;_;
-
-
- /* MODAL CLASS DEFINITION
-	* ====================== */
-
-	var Modal = function (element, options) {
-		this.options = options
-		this.$element = $(element)
-			.delegate('[data-dismiss="modal"]', 'click.dismiss.modal', $.proxy(this.hide, this))
-		this.options.remote && this.$element.find('.modal-body').load(this.options.remote)
-	}
-
-	Modal.prototype = {
-
-			constructor: Modal
-
-		, toggle: function () {
-				return this[!this.isShown ? 'show' : 'hide']()
-			}
-
-		, show: function () {
-				var that = this
-					, e = $.Event('show')
-
-				this.$element.trigger(e)
-
-				if (this.isShown || e.isDefaultPrevented()) return
-
-				this.isShown = true
-
-				this.escape()
-
-				this.backdrop(function () {
-					var transition = $.support.transition && that.$element.hasClass('fade')
-
-					if (!that.$element.parent().length) {
-						that.$element.appendTo(document.body) //don't move modals dom position
-					}
-
-					that.$element
-						.show()
-
-					if (transition) {
-						that.$element[0].offsetWidth // force reflow
-					}
-
-					that.$element
-						.addClass('in')
-						.attr('aria-hidden', false)
-
-					that.enforceFocus()
-
-					transition ?
-						that.$element.one($.support.transition.end, function () { that.$element.focus().trigger('shown') }) :
-						that.$element.focus().trigger('shown')
-
-				})
-			}
-
-		, hide: function (e) {
-				e && e.preventDefault()
-
-				var that = this
-
-				e = $.Event('hide')
-
-				this.$element.trigger(e)
-
-				if (!this.isShown || e.isDefaultPrevented()) return
-
-				this.isShown = false
-
-				this.escape()
-
-				$(document).off('focusin.modal')
-
-				this.$element
-					.removeClass('in')
-					.attr('aria-hidden', true)
-
-				$.support.transition && this.$element.hasClass('fade') ?
-					this.hideWithTransition() :
-					this.hideModal()
-			}
-
-		, enforceFocus: function () {
-				var that = this
-				$(document).on('focusin.modal', function (e) {
-					if (that.$element[0] !== e.target && !that.$element.has(e.target).length) {
-						that.$element.focus()
-					}
-				})
-			}
-
-		, escape: function () {
-				var that = this
-				if (this.isShown && this.options.keyboard) {
-					this.$element.on('keyup.dismiss.modal', function ( e ) {
-						e.which == 27 && that.hide()
-					})
-				} else if (!this.isShown) {
-					this.$element.off('keyup.dismiss.modal')
-				}
-			}
-
-		, hideWithTransition: function () {
-				var that = this
-					, timeout = setTimeout(function () {
-							that.$element.off($.support.transition.end)
-							that.hideModal()
-						}, 500)
-
-				this.$element.one($.support.transition.end, function () {
-					clearTimeout(timeout)
-					that.hideModal()
-				})
-			}
-
-		, hideModal: function (that) {
-				this.$element
-					.hide()
-					.trigger('hidden')
-
-				this.backdrop()
-			}
-
-		, removeBackdrop: function () {
-				this.$backdrop.remove()
-				this.$backdrop = null
-			}
-
-		, backdrop: function (callback) {
-				var that = this
-					, animate = this.$element.hasClass('fade') ? 'fade' : ''
-
-				if (this.isShown && this.options.backdrop) {
-					var doAnimate = $.support.transition && animate
-
-					this.$backdrop = $('<div class="modal-backdrop ' + animate + '" />')
-						.appendTo(document.body)
-
-					this.$backdrop.click(
-						this.options.backdrop == 'static' ?
-							$.proxy(this.$element[0].focus, this.$element[0])
-						: $.proxy(this.hide, this)
-					)
-
-					if (doAnimate) this.$backdrop[0].offsetWidth // force reflow
-
-					this.$backdrop.addClass('in')
-
-					doAnimate ?
-						this.$backdrop.one($.support.transition.end, callback) :
-						callback()
-
-				} else if (!this.isShown && this.$backdrop) {
-					this.$backdrop.removeClass('in')
-
-					$.support.transition && this.$element.hasClass('fade')?
-						this.$backdrop.one($.support.transition.end, $.proxy(this.removeBackdrop, this)) :
-						this.removeBackdrop()
-
-				} else if (callback) {
-					callback()
-				}
-			}
-	}
-
-
- /* MODAL PLUGIN DEFINITION
-	* ======================= */
-
-	var old = $.fn.modal
-
-	$.fn.modal = function (option) {
-		return this.each(function () {
-			var $this = $(this)
-				, data = $this.data('modal')
-				, options = $.extend({}, $.fn.modal.defaults, $this.data(), typeof option == 'object' && option)
-			if (!data) $this.data('modal', (data = new Modal(this, options)))
-			if (typeof option == 'string') data[option]()
-			else if (options.show) data.show()
-		})
-	}
-
-	$.fn.modal.defaults = {
-			backdrop: true
-		, keyboard: true
-		, show: true
-	}
-
-	$.fn.modal.Constructor = Modal
-
-
- /* MODAL NO CONFLICT
-	* ================= */
-
-	$.fn.modal.noConflict = function () {
-		$.fn.modal = old
-		return this
-	}
-
-
- /* MODAL DATA-API
-	* ============== */
-
-	$(document).on('click.modal.data-api', '[data-toggle="modal"]', function (e) {
-		var $this = $(this)
-			, href = $this.attr('href')
-			, $target = $($this.attr('data-target') || (href && href.replace(/.*(?=#[^\s]+$)/, ''))) //strip for ie7
-			, option = $target.data('modal') ? 'toggle' : $.extend({ remote:!/#/.test(href) && href }, $target.data(), $this.data())
-
-		e.preventDefault()
-
-		$target
-			.modal(option)
-			.one('hide', function () {
-				$this.focus()
-			})
-	})
-
-}(window.jQuery);
-
-
-
-
-
 var ui = {
 	/**
 	 * 浮屏显示消息，提示信息框
@@ -276,6 +46,10 @@ var ui = {
 					<span class="timer">'+ lazytime + ' 秒后自动关闭</span>\
 					<button class="btn" data-dismiss="modal" aria-hidden="true">立即关闭</button>\
 					</div></div>';
+
+		// 先清除历史加载的modal
+		$('#uiBox, #uiConfirm, #uiLoad').remove();
+
 		$(html).appendTo(document.body);
 		$('#uiBox').modal();
 
@@ -292,7 +66,6 @@ var ui = {
 		}, 1000);
 
 		$('#uiBox').on('hide', function() {
-			$('#uiBox').remove();
 			lazytime = 0;
 		});
 	},
@@ -342,17 +115,19 @@ var ui = {
 					<a href="javascript:void(0);" class="btn btn-primary btn-ok" data-dismiss="modal" aria-hidden="true">确定</a>\
 					<a href="javascript:void(0);" class="btn btn-cancel" data-dismiss="modal" aria-hidden="true">取消</a>\
 					</div></div>';
+
+		// 先清除历史加载的modal
+		$('#uiBox, #uiConfirm, #uiLoad').remove();
+
 		$(html).appendTo(document.body);
 		$('#uiConfirm').modal();
 
 		$("#uiConfirm .btn-cancel").one('click',function() {
 			$('#uiConfirm').modal('hide');
-			$('#uiConfirm').remove();
 			return false;
 		});
 		$("#uiConfirm .btn-ok").one('click',function() {
 			$('#uiConfirm').modal('hide');
-			$('#uiConfirm').remove();
 			if('undefined' == typeof(callback)) {
 				return true;
 			} else {
@@ -386,24 +161,21 @@ var ui = {
 					</div>\
 					<div class="modal-body"></div>\
 					<div class="modal-footer">\
-					<button class="btn btn-primary btn-ok">确定</button>\
+					<button class="btn btn-primary btn-ok">提交</button>\
 					<button class="btn btn-cancel" data-dismiss="modal" aria-hidden="true">取消</button>\
 					</div></div>';
+		// 先清除历史加载的modal
+		$('#uiBox, #uiConfirm, #uiLoad').remove();
+
 		$(html).appendTo(document.body);
 		$('#uiLoad').modal();
 		$('#uiLoad').find('.modal-body').load(requestUrl);
-
 		$("#uiLoad .btn-cancel").one('click',function() {
 			$('#uiLoad').modal('hide');
-			$('#uiLoad').remove();
 			return false;
 		});
 		$("#uiLoad .btn-ok").one('click',function() {
 			$('#uiLoad .modal-body').find('form').submit();
 		});
 	}
-
-
-
-	
 }
